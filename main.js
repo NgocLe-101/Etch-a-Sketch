1d80605f90715470ce9cb22cbedd2829d1f8ebda
NgocLe-101 <ngocle266tqt@gmail.com>
2024-05-12 17:51:03 +0700

    Add button functionality to get user input and re-render the grid

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-const container = document.querySelector('.container');
+let container = document.querySelector('.container');
 let n = 16;
 let color = 'red';
+const body = document.querySelector('body')
 
-for (let i = 0;i<n;i++) {
-    const row = document.createElement('div');
-    row.className = 'grid-row';
-    for (let j = 0;j<n;j++) {
-        let cell = document.createElement('div');
-        cell.className = 'grid-cell';
-        cell.addEventListener('mouseover', () => {
-            cell.style['background-color'] = color;
-        })
-        row.append(cell);
+function renderGrid(size) {
+    body.removeChild(container);
+    container = document.createElement('div');
+    container.className = 'container';
+    for (let i = 0;i<size;i++) {
+        const row = document.createElement('div');
+        row.className = 'grid-row';
+        for (let j = 0;j<size;j++) {
+            let cell = document.createElement('div');
+            cell.className = 'grid-cell';
+            cell.addEventListener('mouseover', () => {
+                cell.style['background-color'] = color;
+            })
+            row.append(cell);
+        }
+        container.append(row);
     }
-    container.append(row);
+    body.append(container);
 }
+
+const clearButton = document.querySelector('.clear-button');
+clearButton.onclick = () => {
+    let userInput = parseInt(prompt('Enter the grid size: '));
+    while (isNaN(userInput) || userInput < 1 || userInput > 100) {
+        alert("Grid size has to be a number between 1 and 100!")
+        userInput = parseInt(prompt('Enter the grid size: '));
+    }
+    n = userInput;
+    renderGrid(n);
+}
+
+renderGrid(n);
